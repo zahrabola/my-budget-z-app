c9a8d64905d0af4311e9500f508fe50c66fe9c9d
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-04-03 21:07:58 +0100

    Filter expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,36 +1,39 @@
 import React, { useState, useContext, useEffect } from "react";
-import ExpenseItem from './ExpenseItem';
+import ExpenseItem from "./ExpenseItem";
 import { AppContext } from "../context/AppContext";
 
- const ExpenseList = () => {
-   const { expenses } = useContext(AppContext);
-const [filteredExpenses, setfilteredExpenses] = useState(expenses || []);
-useEffect(() => {
-  setfilteredExpenses(expenses);
-}, [expenses]);
-const handleChange = (event) => {
-  const searchResults = expenses.filter((filteredExpense) =>
-    filteredExpense.name.toLowerCase().includes(event.target.value)
-  );
-  setfilteredExpenses(searchResults);
-};
+const ExpenseList = () => {
+  const { expenses } = useContext(AppContext);
+  const [filteredExpenses, setfilteredExpenses] = useState(expenses || []);
+  useEffect(() => {
+    setfilteredExpenses(expenses);
+  }, [expenses]);
+  const handleChange = (event) => {
+    const searchResults = expenses.filter((filteredExpense) =>
+      filteredExpense.name.toLowerCase().includes(event.target.value)
+    );
+    setfilteredExpenses(searchResults);
+  };
 
- return (
-   <>
-     <input
-       type="text"
-       class="form-control mb-2 mr-sm-2"
-       placeholder="searching..."
-       onChange={handleChange}
-     />
-     <ul className="list-group  mt-3 mb-3">
-       {expenses.map((expense) => (
-         <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />
-       ))}
-     </ul>
-   </>
- );
- };
- 
+  return (
+    <>
+      <input
+        type="text"
+        class="form-control mb-2 mr-sm-2"
+        placeholder="searching..."
+        onChange={handleChange}
+      />
+      <ul className="list-group  mt-3 mb-3">
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            id={expense.id}
+            name={expense.name}
+            cost={expense.cost}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default ExpenseList;
